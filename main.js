6c5514eeb8fab233c8ff183018f67785e1c98510
Matheus Fialho <mgfm@cesar.school>
2025-06-09 19:20:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-console.log("Hello World!");
+document.addEventListener('DOMContentLoaded', () => {
+  const linksInternos = document.querySelectorAll('a[href^="#"]');
 
-        
+  linksInternos.forEach(link => {
+    link.addEventListener('click', event => {
+      const href = link.getAttribute('href');
+      const destino = document.querySelector(href);
+
+      if (destino) {
+        event.preventDefault();
+        destino.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start' 
+        });
+      }
+    });
+  });
+});
